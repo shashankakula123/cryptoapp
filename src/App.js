2cafc11589f663b1ef72fa4bbb36287149038dd8
shashankakula123[A[Agit <shashank310oro@gmail.com>
2021-12-15 13:14:55 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Switch,Link,Route} from 'react-router-dom';
+import {Layout,Typography,Space} from 'antd';
+function App() {
+    return (
+        <div>
+            <div className='nav'>
+
+            </div>
+        
+            
+            <div className='main'>
+
+            </div>
+            
+            <div className='footer'>
+
+            </div>
+
+            </div>
+    )
+}
+
+export default App;
